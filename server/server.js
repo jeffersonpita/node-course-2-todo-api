5dfd97881f5ab0c831fb50fc03bfa2e31f42a750
jeffersonpita <jeffersonpita@yahoo.com.br>
2018-07-01 10:41:22 -0300

    update done

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ var {Todo} = require("./models/todo.js");
 var {User} = require("./models/user.js");
 
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 
 app.use(bodyParser.json());
 
