5fe659177de1e3622b5bb24b93caea3d2e37fb80
Jefferson <jeffersonpita@yahoo.com.br>
2018-07-06 13:49:47 -0300

    login

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var _ = require("lodash");
 var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID} = require("mongodb");
+const bcrypt = require("bcryptjs");
 
 
 var {mongoose} = require("./db/mongoose.js");
@@ -111,7 +112,17 @@ app.post('/users', (req, res) => {
   })
 });
 
-
+// POST /users/login
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user)=>{
+      return user.generateAuthToken().then((token)=>{
+        res.header('x-auth', token).send(user);
+      });
+  }).catch((e)=>{
+    res.status(400).send(e);
+  });
+});
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
